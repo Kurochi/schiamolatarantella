d541b4b0c56c2a7fa091ab03385ac09ae3252235
Guido Belluomo <razor994@live.it>
2017-08-22 02:28:01 +0200

    Notifica tarantelle ricevute non risposte

--- awesome/web/profilo/barra_profilo.php
+++ awesome/web/profilo/barra_profilo.php
@@ -1,5 +1,19 @@
+<?php
+require_once "../libs/user.php";
+require_once "../libs/session.php";
+require_once "../libs/tarantella.php";
+
+$_notifica = "";
+if (Session::CheckSession() === SessionStatus::User)
+{
+    $_tarantelle = Tarantella::GetByDestinatario(Session::GetUserID(), false);
+    $_numeroTarantelle = count($_tarantelle);
+    $_notifica = $_numeroTarantelle > 0 ? " - <span style='font-weight: bold; color: lime'>$_numeroTarantelle</span>" : "";
+}
+?>
+
 <ul id="profileBar">
     <li class="topBarButton"><a href="index.php">Pagina Principale</a></li>
-    <li class="topBarButton"><a href="tarantellericevute.php">Tarantelle Ricevute</a></li>
+    <li class="topBarButton"><a href="tarantellericevute.php">Tarantelle Ricevute<?= $_notifica ?></a></li>
     <li class="topBarButton"><a href="tarantelleschiate.php">Tarantelle Schiate</a></li>
 </ul>
